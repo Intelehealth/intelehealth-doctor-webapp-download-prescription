354b94ab4dda4e758334bf53e3ab119707083ccf
shaheen-p <spathan@intelehealth>
2025-11-11 12:23:22 +0530

    feat: removed unwanted text

--- projects/lib-presciption/src/public-api.ts
+++ projects/lib-presciption/src/public-api.ts
@@ -7,6 +7,6 @@ export * from './lib/lib-presciption.component';
 export * from './lib/lib-presciption.module';
 export * from './lib/services/env.service'; // Export the service
 export * from './lib/config/config.token'; // Export the token
-export * from './lib/directives/default-image.directive';
+export * from './lib/directives/default-image.directive'; 
 
 
