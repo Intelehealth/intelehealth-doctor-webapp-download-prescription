962cd0947978288982b6032de8894ea40fbe1f17
shaheen-p <spathan@intelehealth>
2025-11-11 12:26:47 +0530

    feat: updated build

--- lib/lib/config/config.token.d.ts
+++
@@ -1,2 +0,0 @@
-import { InjectionToken } from '@angular/core';
-export declare const ENV_CONFIG: InjectionToken<any>;
--- lib/public-api.d.ts
+++
@@ -1,6 +0,0 @@
-export * from './lib/lib-presciption.service';
-export * from './lib/lib-presciption.component';
-export * from './lib/lib-presciption.module';
-export * from './lib/services/env.service';
-export * from './lib/config/config.token';
-export * from './lib/directives/default-image.directive';
